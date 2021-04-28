b6dfb657bd9c10faa39dba489b3135d05e513ecd
David Yem <david13200arles@hotmail.fr>
2021-04-28 21:23:18 +0200

    produit

---
+++ js/produit.js
@@ -0,0 +1,30 @@
+const TeddieUrl = "http://localhost:3000/api/teddies/"
+const id = localStorage.getItem("id")
+console.log(id)
+
+getProduit(TeddieUrl+id)
+
+function getProduit(url){
+    fetch(url)
+        .then(function(res){
+            if(res.ok){
+                return res.json()
+            }
+        })
+        .then(function(response){
+            console.log(response)
+            let photo = document.createElement("img")
+            photo.classList.add("produit-photo__image")
+            document.getElementById("photo").appendChild(photo)
+            photo.src = response.imageUrl
+            let description = document.getElementById("description")
+            description.innerHTML = `Description du produit:<br> ${response.description}`
+            let prix = document.getElementById("prix")
+            prix.innerHTML = `Prix:<br>${response.price} €`
+            
+
+        })
+        .catch(function(err){
+            alert(err)
+        })
+}
