9b23fcfb34976e2d5026889dfb1dd3ef295a6f0b
David Yem <david13200arles@hotmail.fr>
2021-04-29 02:57:41 +0200

    panier 1ere partie

---
+++ js/panier.js
@@ -0,0 +1,28 @@
+let listePanier = document.getElementById("panier")
+const id = localStorage.getItem("id")
+
+let retour = document.getElementById("retour")
+retour.href = "./produit.html?id="+id
+
+function total(){
+
+    for(let i = 0; i < localStorage.length;i++){
+        localStorage.removeItem("id")
+        let article = localStorage.key(i)
+        let commande = localStorage.getItem(article)
+        let js = JSON.parse(commande)
+        let div = document.createElement("div")
+        div.classList.add("detail")
+        let produit = document.createElement("p")
+        produit.classList.add("detail__produit")
+        let panier = document.getElementById("panier")
+        div.appendChild(produit)
+        panier.appendChild(div)
+        produit.innerHTML = "Nom: "+js.nom+"<br>description: "+js.description+"<br><br>Prix: "+js.prix+" €<br>Quantité: "+js.quantite
+     }
+    localStorage.setItem("id",id)
+}
+        
+
+
+total()
