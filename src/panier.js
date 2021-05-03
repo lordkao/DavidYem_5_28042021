9a980a401c631ce7b7ee5770ac6d1851b847ca67
David Yem <david13200arles@hotmail.fr>
2021-05-03 22:42:35 +0200

    conditions pour envoi de commande

--- src/panier.js
+++ src/panier.js
@@ -6,7 +6,7 @@ retour.href = "./produit.html?id="+id
 let myTotal = 0
 let panier = document.getElementById("panier")
 let commande = JSON.parse(localStorage.getItem("panier"))
-let tableProduits = localStorage.getItem("panier")
+let products = []
 
 function total(){
     if(localStorage.getItem("panier") === null){
@@ -17,6 +17,8 @@ function total(){
     else{
         for (let article of commande){/*Boucle dans le tableau "commande" en créant des éléments html qui vont contenir les informations de la commande */
 
+            products.push(article._id)
+            
             let multi = (article.prix*article.quantite)/*Prix total pour une selection de produit en fonction de la quantité */
 
             let div = document.createElement("div")
@@ -50,60 +52,24 @@ function total(){
     }
 }
 total()
-
-
+console.log(products)
 /*Formulaire*/
 let firstName = document.getElementById("firstName")
-firstName.addEventListener("input",(e)=>{
-    firstName.value = e.target.value
-    console.log(firstName.value)
-})
 let lastName = document.getElementById("lastName")
-lastName.addEventListener("input",(e)=>{
-    lastName.value = e.target.value
-    console.log(lastName.value)
-})
 let address = document.getElementById("address")
-address.addEventListener("input",(e)=>{
-    address.value = e.target.value
-    console.log(address.value)
-})
 let city = document.getElementById("city")
-city.addEventListener("input",(e)=>{
-    city.value = e.target.value
-    console.log(city.value)
-})
 let email = document.getElementById("email")
-email.addEventListener("input",(e)=>{
-email.value = e.target.value
-console.log(email.value)
-})
-
-
-let submit = document.getElementById("submit")
 let form = document.getElementById("form")
+let confirmation = document.getElementById("confirmation")
 
-let contact = {
-    firstName: "david",
-    lastName: "yem",
-    address: "11 rue jean",
-    city: "arles",
-    email: "david@hotmail.fr"
-}
-let objetContact = {
-    contact:contact,
-    tableau:commande
-}
-console.log(objetContact)
-submit.addEventListener("click",(e)=>{
-    e.preventDefault()
-    fetch(TeddieUrl,{
+function send(url,formulaire,id){
+    fetch(url,{
         method:"POST",
         headers:{
             "Accept":"application/json",
             "Content-type":"application/json"
         },
-        body: JSON.stringify(commande)
+        body: JSON.stringify({contact:formulaire,products:id})
     })
     .then(function(res){
         if(res.ok){
@@ -111,12 +77,45 @@ submit.addEventListener("click",(e)=>{
         }
     })
     .then(function(response){
+        localStorage.setItem("confirmation",JSON.stringify(response))
         console.log(response)
     })
     .catch(function(err){
         alert(err)
     })
-    
+}
+let contact
+form.addEventListener("submit",function(e){
+    e.preventDefault()
+    /*Création du formulaire contact en enregistrant les informations du client*/
+    contact = {
+        firstName : firstName.value,
+        lastName : lastName.value,
+        address : address.value,
+        city : city.value,
+        email: email.value
+    }
+    console.log(contact)
+
+    console.log("informations enregistrées")
 })
-console.log(commande)
+confirmation.addEventListener("click",function(e){
+    
+    if(contact === undefined && products.length === 0){
+        e.preventDefault()
+        alert("Veuillez choisir un article avant de passer votre commande.")
+    }
+    else if(products.length === 0){
+        e.preventDefault()
+        alert("Vous n'avez pas encore choisis d'articles")
+    }
+    else if(contact ===undefined){
+        e.preventDefault()
+        alert("Veillez à bien renseigner le formulaire et le valider,merci.")
+    }
+    else if(contact !== undefined && products.length !== 0){
+    send(TeddieUrl,contact,products)
+    }
+})
+console.log(products)
 //firstName, lastName, address, city et email
