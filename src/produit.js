c128346136d1adc2fc58151ba507e334c0671880
David Yem <david13200arles@hotmail.fr>
2021-05-03 13:11:51 +0200

    quantité affichée

--- src/produit.js
+++ src/produit.js
@@ -1,13 +1,11 @@
 const TeddieUrl = "http://localhost:3000/api/teddies/"
 const id = localStorage.getItem("id")
 let panier = []
-getPanier("panier")
-
-let produit /*Ici produit aura pour valeur la réponse à la requête passée dans la fonction getProduit ligne 59. */
-
+let produit /*Ici produit aura pour valeur la réponse à la requête passée dans la fonction getProduit ligne 62. */
 let ajouter = document.getElementById("ajouter")
 let quantite = document.getElementById("quantite")
-
+getProduit(TeddieUrl+id)
+getPanier("panier")
 
 console.log(id)
 
@@ -16,41 +14,48 @@ function getPanier(value){
 
     if(localStorage.getItem(value)!=null){
         panier = JSON.parse(localStorage.getItem("panier"))
-        /*console.log('il y déjà un panier!!')
-        console.log(panier)*/
+        console.log('il y déjà un panier!!')
+        console.log(panier)
     }
     else{
         console.log('il faut créer un panier')
     }
 }
-
-function getInfosproduit(produit){
+function getInfosproduit(article){
     let photo = document.createElement("img")
     photo.classList.add("produit-photo__image")
     document.getElementById("photo").appendChild(photo)
-    photo.src = produit.imageUrl
+    photo.src = article.imageUrl
     let nom = document.getElementById("nom")
-    nom.innerText = produit.name
+    nom.innerText = article.name
     let description = document.getElementById("description")
-    description.innerHTML = `Description du produit:<br> ${produit.description}`
+    description.innerHTML = `Description du produit:<br> ${article.description}`
     let prix = document.getElementById("prix")
-    prix.innerHTML = `Prix:<br>${produit.price} €`
+    prix.innerHTML = `Prix:<br>${article.price} €`
 }
-function createSelect(produit){
+function createSelect(responseRequest){
     let select = document.getElementById("select")
-            let selectDefault = document.createElement("option")
-            selectDefault.innerText = "couleur"
-            select.appendChild(selectDefault)
+    let selectDefault = document.createElement("option")
+    selectDefault.innerText = "couleur"
+    select.appendChild(selectDefault)
 
-             /*Création de l'élément de selection de couleur,boucle dans le array colors et ajoute la couleur de l'itération dans un nouvel élément <option> de <select>*/
-            for(let color of produit.colors){
-                let couleur = document.createElement("option")
-                select.appendChild(couleur)
-                couleur.innerText = color 
-                couleur.value = color
-            }
+    /*Création de l'élément de selection de couleur,boucle dans le array colors et ajoute la couleur de l'itération dans un nouvel élément <option> de <select>*/
+    for(let color of responseRequest.colors){
+        let couleur = document.createElement("option")
+        select.appendChild(couleur)
+        couleur.innerText = color 
+        couleur.value = color
+    }
 }
-function getProduit(url){
+function getQuantityPanier(responseRequest){
+    let notreProduit = panier.find(elt => elt.nom === responseRequest.name)
+        if(notreProduit){
+            quantite.value = notreProduit.quantite  
+            console.log(notreProduit)
+            console.log(quantite.value)
+        }
+}
+async function getProduit(url){
     fetch(url)
         .then(function(res){
             if(res.ok){
@@ -59,64 +64,20 @@ function getProduit(url){
         })
         .then(function(response){
             produit = response
-            nomProduit = response.name
 
             console.log(response)/*affiche dans la console les informations contenues dans un objet*/
             
             getInfosproduit(response)/*Récupération des informations concernant le produit selectionné */
 
-            /*Création de l'élément de selection de couleur par défaut "couleur"*/
-            
-            createSelect(response)
+            createSelect(response)/*Création de l'élément de selection de couleur par défaut "couleur"*/
 
-            /*let ajouter = document.getElementById("ajouter")
-            let quantite = document.getElementById("quantite")
-            let nomDuProduit = JSON.parse(localStorage.getItem(response.name))
-
-            console.log(nomDuProduit)*/
-
-            /*Si un objet du produit existe alors on récupère sa quantité enregistré*/
-
-            /*if(localStorage.getItem(response.name) != null){
-            quantite.value = nomDuProduit.quantite
-            console.log(quantite.value)
-            }*/
-            
-            /*A chaque clique du bouton "Ajouter au panier",création d'un objet dans le localStorage ayant pour clé le nom du produit*/
-            /*ajouter.addEventListener("click",(e)=>{
-                e.preventDefault()
-
-                if(quantite.value != 0 && quantite.value >=0){
-
-               
-                let commande = {
-                    nom: response.name,
-                    quantite: quantite.value,
-                    prix: response.price,
-                    image: response.imageUrl,
-                    description: response.description
-                    }
-                panier.push(commande)
-                    console.log(panier.length)
-                    console.log(panier)
-                    localStorage.setItem("panier",JSON.stringify(panier))
-                    /*localStorage.setItem(response.name,JSON.stringify(commande))
-                }
-                else if(quantite.value ==0){
-                    localStorage.removeItem("panier")
-                }
-                else{
-                    alert("veuillez renseigner une quantité valide")
-                }
-            })*/
+            getQuantityPanier(response)/*Obtenir la quantité déjà déclaré si elle existe et l'affichée */
         })
         .catch(function(err){
             alert(err)
         })
 }
 /*Appel de la fonction getProduit pour construire le contenu de la page avec les infos du produit selectionné sur la page index */
-getProduit(TeddieUrl+id)
-
 class Produit{
     constructor(nom,quantite,prix,image,description){
         this.nom = nom,
@@ -129,7 +90,6 @@ class Produit{
         return this.quantite = value
     }
 }
-
 ajouter.addEventListener("click",(e)=>{
     e.preventDefault()
     let eltTrouve = panier.find(elt => elt.nom === produit.name)/*Recherche dans le panier si l'élément existe */
@@ -165,9 +125,8 @@ ajouter.addEventListener("click",(e)=>{
         if(eltTrouve){
             panier.splice(index,1)
             localStorage.setItem("panier",JSON.stringify(panier))
+            console.log(panier)
         }
-        console.log(panier)
-        
         alert("quantité inférieure ou égale à 0")
     }
     else{
